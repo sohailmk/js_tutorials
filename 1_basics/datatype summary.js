2d1d200b54c88f02a44d0bc7ae28f0bf4962485f
sohailmk <sohailmukhtar369@gmail.com>
2025-02-20 11:33:24 +0000

    dtsummary

--- 1_basics/datatype summary.js
+++ 1_basics/datatype summary.js
@@ -35,3 +35,26 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// --------------------------------------------
+// Stack(Premitive) & Heap(Non-primitive)
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+
+anothername "chaiaurcode"
+
+console.log(my Youtubename);
+console.log(anothername);
+
+let userOne = {
+
+email: "user@google.com",
+upi: "user@ybl"
+}
+let userTwo = userOne
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
